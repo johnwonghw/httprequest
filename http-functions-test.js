0a04625fa68609cb96f1aed4235f42a7985ca04f
John <john.wong.h@gmail.com>
2017-07-03 21:10:07 +0000

    Completed Step 5. Made function into a module

---
+++ http-functions-test.js
@@ -0,0 +1,15 @@
+var https = require('https');
+
+var getHTML = require('./http-functions');
+
+var options = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML (options, printHTML)
---
+++ http-functions.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+module.exports = function getHTML (options, callback) {
+
+
+    https.get(options, function (response) {
+
+      response.setEncoding('utf8');
+
+      let rawData = '';
+      response.on ('data', function (chunk) {
+        rawData += chunk;
+        console.log (chunk, "\n");
+      });
+
+      response.on('end', function() {
+        console.log('Response stream complete.');
+        callback(rawData)
+      });
+    });
+  }
+
+  function printHTML (html) {
+    console.log(html);
+  }
+
+  var options = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
